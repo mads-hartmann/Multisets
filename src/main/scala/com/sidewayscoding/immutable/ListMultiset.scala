a23f003099ea53f12d2925edbfc570dd69187513
Mads Hartmann Jensen <mads379@gmail.com>
2012-09-04 19:51:32 +0200

    Moved code around a bit.
    The ListMultisetIterator and ListMultisetBuilder were cluttering
    the companion object a bit.

--- src/main/scala/com/sidewayscoding/immutable/ListMultiset.scala
+++ src/main/scala/com/sidewayscoding/immutable/ListMultiset.scala
@@ -12,7 +12,7 @@ import scala.collection.immutable.{ TreeMap }
  *  than one that has no obvious bugs) so it can be used to test more advanced
  *  implementations.
  */
-class ListMultiset[A : Ordering] private (val delegate: TreeMap[A, Int]) extends MultisetLike[A, ListMultiset[A]] {
+class ListMultiset[A : Ordering] private[immutable] (val delegate: TreeMap[A, Int]) extends MultisetLike[A, ListMultiset[A]] {
 
   import ListMultiset._
 
@@ -61,48 +61,48 @@ object ListMultiset {
   def apply[A : Ordering](): ListMultiset[A] =
     new ListMultiset[A](TreeMap[A,Int]())
 
-  class ListMultisetBuilder[A : Ordering] extends Builder[A, ListMultiset[A]] {
-    private var delegate: TreeMap[A, Int] = new TreeMap[A,Int]()
-
-    def +=(a: A): this.type = {
-      val newCount = delegate.get(a).map(_ + 1).getOrElse(1)
-      delegate = delegate.updated(a, newCount)
-      this
-    } 
-    
-    def clear() = { delegate = new TreeMap[A,Int]() }
-    
-    def result = { new ListMultiset[A](delegate) }
-  }
-
-  class ListMultisetIterator[A : Ordering](private val tm: TreeMap[A, Int]) extends Iterator[A] {
-
-    private[this] val mapIterator = tm.iterator
-    private[this] var indexInElement = 1
-    private[this] var currentElementCount: Int = 0
-    private[this] var currentElement: Option[A] = None
-
-    override def hasNext: Boolean = {      
-      indexInElement < currentElementCount || mapIterator.hasNext
-    }
-    
-    override def next(): A = {
-      if (indexInElement < currentElementCount) {
-        indexInElement += 1
-        return currentElement.get
-      } else {
-        if (mapIterator.hasNext) {
-          val (e,c) = mapIterator.next()
-          currentElement = Some(e)
-          currentElementCount = c
-          indexInElement = 1
-          return e
-        } else {
-          throw new NoSuchElementException("next on empty iterator")
-        }      
-      }
-    }
-  } 
-
 }
+
+class ListMultisetBuilder[A : Ordering] extends Builder[A, ListMultiset[A]] {
+  private var delegate: TreeMap[A, Int] = new TreeMap[A,Int]()
+
+  def +=(a: A): this.type = {
+    val newCount = delegate.get(a).map(_ + 1).getOrElse(1)
+    delegate = delegate.updated(a, newCount)
+    this
+  } 
+  
+  def clear() = { delegate = new TreeMap[A,Int]() }
+  
+  def result = { new ListMultiset[A](delegate) }
+}
+
+class ListMultisetIterator[A : Ordering](private val tm: TreeMap[A, Int]) extends Iterator[A] {
+
+  private[this] val mapIterator = tm.iterator
+  private[this] var indexInElement = 1
+  private[this] var currentElementCount: Int = 0
+  private[this] var currentElement: Option[A] = None
+
+  override def hasNext: Boolean = {      
+    indexInElement < currentElementCount || mapIterator.hasNext
+  }
+  
+  override def next(): A = {
+    if (indexInElement < currentElementCount) {
+      indexInElement += 1
+      return currentElement.get
+    } else {
+      if (mapIterator.hasNext) {
+        val (e,c) = mapIterator.next()
+        currentElement = Some(e)
+        currentElementCount = c
+        indexInElement = 1
+        return e
+      } else {
+        throw new NoSuchElementException("next on empty iterator")
+      }      
+    }
+  }
+} 
 
