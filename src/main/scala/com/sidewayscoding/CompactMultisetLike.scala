c39e3d417753eba76e14f4cc34321c50491b5b7a
Mads Hartmann Jensen <mads379@gmail.com>
2012-10-21 19:59:51 +0200

    Documented the method of FullMultisetLike a bit. #4

--- src/main/scala/com/sidewayscoding/CompactMultisetLike.scala
+++ src/main/scala/com/sidewayscoding/CompactMultisetLike.scala
@@ -1,5 +1,11 @@
 package com.sidewayscoding
 
+/**
+ * A Compact Multiset is a Multiset that will only store one of each equal element and use a
+ * number to keep track of the multiplicity of each element. This is useful to safe memory if 
+ * the equals method is defined over all of the properties of the elements (as is the default 
+ * for case classes) 
+ */
 trait CompactMultisetLike[A, +This <: CompactMultiset[A] with CompactMultisetLike[A, This]] extends MultisetLike[A, This] {
 
 }
--- src/main/scala/com/sidewayscoding/FullMultisetLike.scala
+++ src/main/scala/com/sidewayscoding/FullMultisetLike.scala
@@ -1,8 +1,41 @@
 package com.sidewayscoding
 
+/**
+ * A FullMultiset is a Multiset that keeps all occurrences of the elements in memory. This is useful
+ * if the equals mehtod isn't defined over all of the properties of the elements as you won't loose
+ * any information.
+ */
 trait FullMultisetLike[A, +This <: FullMultiset[A] with FullMultisetLike[A, This]] extends MultisetLike[A, This] {
+
+  /**
+   * Returns a Map with each distinct element mapping to the sequences of elements that are
+   * equal to it.
+   */
   def copies: Map[A, Seq[A]]
+
+  /**
+   * Returns all the occurrences of `a`.
+   */
   def get(a: A): Seq[A]
+
+  /**
+   * Removes the first element in this multiset that are equal to `a`with respect
+   * to the `equals` method defined by `eq`.
+   * 
+   * The `equals` method defined by `eq` must be more strict than the equals method
+   * defined by the element, that is, for any elements X and Y if X.equals(Y) is true
+   * than eq.equals(X,Y) must also return true but not vice-versa.
+   * 
+   */
   def removed(a: A, eq: Equiv[A]): This
+
+  /**
+   * Removes all elements in this multiset that are equal to `a`with respect
+   * to the `equals` method defined by `eq`.
+   * 
+   * The `equals` method defined by `eq` must be more strict than the equals method
+   * defined by the element, that is, for any elements X and Y if X.equals(Y) is true
+   * than eq.equals(X,Y) must also return true but not vice-versa.
+   */
   def removedAll(a: A, eq: Equiv[A]): This
 }
