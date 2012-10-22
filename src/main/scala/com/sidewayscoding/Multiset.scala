b2e1cd162df89f9c882e3b286d2256d3f7e28ebe
Mads Hartmann Jensen <mads379@gmail.com>
2012-10-22 13:25:59 +0200

    A bit of documentation is always nice

--- src/main/scala/com/sidewayscoding/Multiset.scala
+++ src/main/scala/com/sidewayscoding/Multiset.scala
@@ -4,6 +4,21 @@ import scala.collection.GenIterable
 import scala.collection.generic.GenericCompanion
 import scala.collection.generic.CanBuildFrom
 
+/**
+ * Multisets are sets that allow multiple occurrences of the same element
+ *
+ * {{{
+ * val ms = Multiset(1,2,3,3,2)
+ * ms.multiplicity(3) // 2
+ * ms.size() // 5
+ * }}}
+ *
+ * A distinction is made bewtween Multisets that keep all elements in memory (`FullMultiset`) and
+ * those that store only the first of equal elements and use a counter to keep
+ * track of the multiplicity (`CompactMultiset`). This distinction is important if you have defined an
+ * equals method that does not consider all properties of the elements as you would
+ * then loose imformation if you only stored the first of equal elements.
+ */
 trait Multiset[A] extends Iterable[A]
                      with MultisetLike[A, Multiset[A]]
                      with GenIterable[A]
