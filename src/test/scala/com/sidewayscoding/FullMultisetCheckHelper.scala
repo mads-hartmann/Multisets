969abc7d7401011e385a8a8dc2ec5f2248f1a2c5
Mads Hartmann Jensen <mads379@gmail.com>
2012-10-24 11:06:10 +0200

    Structuring tests better. I now get tests for ALL FullHMultisets for free!

--- src/test/scala/com/sidewayscoding/FullMultisetCheckHelper.scala
+++ src/test/scala/com/sidewayscoding/FullMultisetCheckHelper.scala
@@ -1,16 +1,21 @@
-package com
+package com.sidewayscoding
 
 import org.scalacheck.util.Buildable
 import org.scalacheck.Gen
 import org.scalacheck.Arbitrary
-import org.scalacheck.Arbitrary$
 
-package object sidewayscoding {
+trait FullMultisetCheckHelper {
+
+  type T[A] <: FullMultiset[A] with MultisetLike[A, T[A]]
+
+  def empty[A]: T[A]
+
+  def create[A](as: A*): T[A]
 
   case class TestPerson(name: String, age: Int)
 
-  implicit def buildableMultiset[A] = new Buildable[A, FullMultiset] {
-    def builder = new MultisetBuilder(FullMultiset.empty[A])
+  implicit def buildableMultiset[A] = new Buildable[A, T] {
+    def builder = new MultisetBuilder(empty[A])
   }
 
   val arbitraryPerson = for {
@@ -18,8 +23,13 @@ package object sidewayscoding {
     age <- Arbitrary.arbitrary[Int]
   } yield TestPerson(name, age)
 
-  val genStringMultiset = Gen.containerOf[FullMultiset,String](Gen.alphaStr)
-  val genPeopleMultiset = Gen.containerOf[FullMultiset,TestPerson](arbitraryPerson)
+  val genStringMultiset = Gen.containerOf[T, String](Gen.alphaStr)
+  val genPeopleMultiset = Gen.containerOf[T, TestPerson](arbitraryPerson)
+
+  val genMultisetAndString = for {
+    mset <- genStringMultiset
+    s <- Arbitrary.arbitrary[String]
+  } yield (mset, s)
 
   /**
    * Generates a tuple with a Multiset with arbitrary strings and a string that is
@@ -27,7 +37,7 @@ package object sidewayscoding {
    */
   val genMultisetWithString = for {
     mset <- genStringMultiset
-       s <- Arbitrary.arbitrary[String]
+    s <- Arbitrary.arbitrary[String]
   } yield (mset + s, s)
 
   /**
@@ -37,7 +47,7 @@ package object sidewayscoding {
   val genMultisetWithMap = for {
     strs <- Gen.listOfN(100, Gen.alphaStr)
     map = strs.groupBy(id => id)
-    mset = FullMultiset(strs:_*)
+    mset = create(strs: _*)
   } yield (mset, map)
 
   /**
@@ -48,7 +58,7 @@ package object sidewayscoding {
     str <- Arbitrary.arbitrary[String]
     strs <- Gen.listOfN(100, Gen.alphaStr)
     map = (str :: strs).groupBy(id => id)
-    mset = FullMultiset((str :: strs):_*)
+    mset = create((str :: strs): _*)
   } yield (mset, map, str)
 
   /**
@@ -58,17 +68,18 @@ package object sidewayscoding {
   val genMultisetWithIntMap = for {
     strs <- Gen.listOfN(100, Gen.alphaStr)
     map = strs.groupBy(id => id).mapValues(_.size)
-    mset = FullMultiset(strs:_*)
-  } yield (mset,map)
+    mset = create(strs: _*)
+  } yield (mset, map)
 
-  val peopleMultisetContainingPerson: Gen[(FullMultiset[TestPerson], TestPerson)] = for {
-       p <- arbitraryPerson
+  val peopleMultisetContainingPerson: Gen[(T[TestPerson], TestPerson)] = for {
+    p <- arbitraryPerson
     mset <- genPeopleMultiset
   } yield (mset + p, p)
 
-  val peopleMultisetContainingPeople: Gen[(FullMultiset[TestPerson], TestPerson)] = for {
+  val peopleMultisetContainingPeople: Gen[(T[TestPerson], TestPerson)] = for {
     mset <- genPeopleMultiset
-       p <- arbitraryPerson
+    p <- arbitraryPerson
     val all = mset + p + p
   } yield (all, p)
+
 }
--- src/test/scala/com/sidewayscoding/immutable/FullMultisetCheck.scala
+++ src/test/scala/com/sidewayscoding/immutable/FullMultisetCheck.scala
@@ -7,84 +7,105 @@ import org.scalacheck.Gen._
 import Arbitrary.arbitrary
 import org.scalacheck.util.Buildable
 import com.sidewayscoding.MultisetBuilder
+import com.sidewayscoding.MultisetLike
+import com.sidewayscoding.FullMultisetCheckHelper
 
-object FullMultisetCheck extends Properties("immutable.FullMultiset") {
+object MutableFullHashMultisetAsImmutable extends Properties("mutable.FullHashMultiset")
+                                             with FullMultisetCheckHelper 
+                                             with ImmutableFullMultisetProperties {
 
-  import com.sidewayscoding._
+  type T[A] = com.sidewayscoding.mutable.FullHashMultiset[A]
+  def empty[A] = com.sidewayscoding.mutable.FullHashMultiset.empty[A]
+  def create[A](as: A*) = com.sidewayscoding.mutable.FullHashMultiset.apply(as:_*)
+
+}
+
+object ImmutableFullHashMultiset extends Properties("immutable.FullHashMultiset")
+                                    with FullMultisetCheckHelper
+                                    with ImmutableFullMultisetProperties {
+
+  type T[A] = com.sidewayscoding.immutable.FullHashMultiset[A]
+  def empty[A] = com.sidewayscoding.immutable.FullHashMultiset.empty[A]
+  def create[A](as: A*) = com.sidewayscoding.immutable.FullHashMultiset.apply(as:_*)
+
+}
+
+trait ImmutableFullMultisetProperties {
+
+  this: FullMultisetCheckHelper with Properties =>
 
   property("size") = forAll { (elems: List[String]) =>
     FullMultiset(elems: _*).size == elems.size
   }
 
-  property("adding an item should increase the size of the multiset") = forAll {
-    (ms: FullMultiset[String], s: String) =>
-      (ms + s).size == ms.size + 1
+  property("adding an item should increase the size of the multiset") = forAll(genMultisetAndString) {
+    ((ms: this.T[String], s: String) =>
+      (ms + s).size == ms.size + 1).tupled
   }
 
-  property("adding an item should increase it's multiplicity") = forAll {
-    (ms: FullMultiset[String], s: String) =>
-      (ms + s).multiplicity(s) == ms.multiplicity(s) + 1
+  property("adding an item should increase it's multiplicity") = forAll(genMultisetAndString) {
+    ((ms: this.T[String], s: String) =>
+      (ms + s).multiplicity(s) == ms.multiplicity(s) + 1).tupled
   }
 
   property("removing existing item should decrease size") = forAll(genMultisetWithString) {
-    ((ms: FullMultiset[String], s: String) =>
+    ((ms: this.T[String], s: String) =>
       ((ms.removed(s)).size) == (ms.size - 1) ).tupled
   }
 
   property("removing existing item should decrease multiplicity") = forAll(genMultisetWithString) {
-    ((ms: FullMultiset[String], s: String) =>
+    ((ms: this.T[String], s: String) =>
       (ms.removed(s)).multiplicity(s) == ms.multiplicity(s) - 1 ).tupled
   }
 
-  property("intersection with empty multiset") = forAll {
-    (ms: FullMultiset[String]) =>
+  property("intersection with empty multiset") = forAll(genStringMultiset) {
+    (ms: this.T[String]) =>
       ms.intersect(FullMultiset[String]()).size == 0
   }
 
-  property("union with empty FullMultiset ") = forAll {
-    (ms: FullMultiset[String]) =>
+  property("union with empty FullMultiset") = forAll(genStringMultiset) {
+    (ms: this.T[String]) =>
       ms.union(FullMultiset [String]()).size == ms.size
   }
 
   property("copies") = forAll(genMultisetWithMap) {
-    ((ms: FullMultiset[String], map: Map[String, List[String]]) =>
+    ((ms: this.T[String], map: Map[String, List[String]]) =>
       ms.copies == map).tupled
   }
 
   property("get") = forAll(genMultisetWithMapContainingStr) {
-    ((ms: FullMultiset[String], map: Map[String, List[String]], s: String) =>
+    ((ms: this.T[String], map: Map[String, List[String]], s: String) =>
       ms.get(s) == map.getOrElse(s, Nil)).tupled
   }
 
   property("multiplicities") = forAll(genMultisetWithIntMap) {
-    ((ms: FullMultiset[String], map: Map[String, Int]) =>
+    ((ms: this.T[String], map: Map[String, Int]) =>
       ms.multiplicities == map).tupled
   }
 
   property("removed") = forAll(peopleMultisetContainingPerson) {
-    ((ms: FullMultiset[TestPerson], p: TestPerson) =>
+    ((ms: this.T[TestPerson], p: TestPerson) =>
       ms.removed(p).multiplicity(p) == (ms.multiplicity(p) - 1) ).tupled
   }
 
   // TODO: use special Equiv[TestPerson]
   property("removed with equiv") = forAll(peopleMultisetContainingPerson) {
-    ((ms: FullMultiset[TestPerson], p: TestPerson) =>
+    ((ms: this.T[TestPerson], p: TestPerson) =>
       ms.removed(p).multiplicity(p) == (ms.multiplicity(p) - 1) ).tupled
   }
 
   property("removedAll") = forAll(peopleMultisetContainingPeople)(t => {
-      val (ms: FullMultiset[TestPerson], p: TestPerson) = t
+      val (ms: this.T[TestPerson], p: TestPerson) = t
       (ms.multiplicity(p) > 0)                :| "prop #1" &&
       (ms.removedAll(p).multiplicity(p) == 0) :| "prop #2"
     })
 
   // TODO: use special Equiv[TestPerson]
   property("removedAll with equiv") = forAll(peopleMultisetContainingPeople) {
-    ((ms: FullMultiset[TestPerson], p: TestPerson) => {
+    ((ms: this.T[TestPerson], p: TestPerson) => {
       (ms.multiplicity(p) > 1)                :| "prop #1" &&
       (ms.removedAll(p).multiplicity(p) == 0) :| "prop #2" }).tupled
   }
 
   // TODO: De Morgan's Law
-
 }
