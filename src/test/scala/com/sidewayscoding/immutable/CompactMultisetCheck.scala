cdb86b210337dbfa7b664cf2b99b5c94f477d537
Mads Hartmann Jensen <mads379@gmail.com>
2012-11-01 10:03:55 +0100

    More test refactoring

--- src/test/scala/com/sidewayscoding/immutable/CompactMultisetCheck.scala
+++ src/test/scala/com/sidewayscoding/immutable/CompactMultisetCheck.scala
@@ -5,12 +5,32 @@ import org.scalacheck.Gen._
 import com.sidewayscoding.MultisetCheckHelper
 import com.sidewayscoding.MultisetProperties
 
-object ImmutableCompactHashMultiset extends Properties("immutable.CompactHashMultiset")
+object ImmutableCompactHashMultisetCheck extends Properties("immutable.CompactHashMultiset")
                                        with MultisetCheckHelper
                                        with MultisetProperties {
 
   type T[A] = com.sidewayscoding.immutable.CompactListMultiset[A]
   def empty[A] = com.sidewayscoding.immutable.CompactListMultiset.empty[A]
   def create[A](as: A*) = com.sidewayscoding.immutable.CompactListMultiset.apply(as:_*)
+
+}
+
+object ImmutableCompactMultisetCheck extends Properties("immutable.CompactMultiset")
+                                       with MultisetCheckHelper
+                                       with MultisetProperties {
+
+  type T[A] = com.sidewayscoding.immutable.CompactMultiset[A]
+  def empty[A] = com.sidewayscoding.immutable.CompactMultiset.empty[A]
+  def create[A](as: A*) = com.sidewayscoding.immutable.CompactMultiset.apply(as:_*)
+
+}
+
+object CompactMultisetCheck extends Properties("CompactMultiset")
+                          with MultisetCheckHelper
+                          with MultisetProperties {
+
+  type T[A] = com.sidewayscoding.CompactMultiset[A]
+  def empty[A] = com.sidewayscoding.CompactMultiset.empty[A]
+  def create[A](as: A*) = com.sidewayscoding.CompactMultiset.apply(as:_*)
 
 }
--- src/test/scala/com/sidewayscoding/immutable/FullMultisetCheck.scala
+++ src/test/scala/com/sidewayscoding/immutable/FullMultisetCheck.scala
@@ -11,12 +11,32 @@ import com.sidewayscoding.MultisetLike
 import com.sidewayscoding.MultisetCheckHelper
 import com.sidewayscoding.FullMultisetProperties
 
-object ImmutableFullHashMultiset extends Properties("immutable.FullHashMultiset")
-                                    with MultisetCheckHelper
-                                    with FullMultisetProperties {
+object ImmutableFullHashMultisetCheck extends Properties("immutable.FullHashMultiset")
+                                         with MultisetCheckHelper
+                                         with FullMultisetProperties {
 
   type U[A] = com.sidewayscoding.immutable.FullHashMultiset[A]
   def empty[A] = com.sidewayscoding.immutable.FullHashMultiset.empty[A]
   def create[A](as: A*) = com.sidewayscoding.immutable.FullHashMultiset.apply(as:_*)
+
+}
+
+object ImmutableFullMultisetCheck extends Properties("immutable.FullMultiset")
+                                     with MultisetCheckHelper
+                                     with FullMultisetProperties {
+
+  type U[A] = com.sidewayscoding.immutable.FullMultiset[A]
+  def empty[A] = com.sidewayscoding.immutable.FullMultiset.empty[A]
+  def create[A](as: A*) = com.sidewayscoding.immutable.FullMultiset.apply(as:_*)
+
+}
+
+object FullMultisetCheck extends Properties("FullMultiset")
+                            with MultisetCheckHelper
+                            with FullMultisetProperties {
+
+  type U[A] = com.sidewayscoding.FullMultiset[A]
+  def empty[A] = com.sidewayscoding.FullMultiset.empty[A]
+  def create[A](as: A*) = com.sidewayscoding.FullMultiset.apply(as:_*)
 
 }
