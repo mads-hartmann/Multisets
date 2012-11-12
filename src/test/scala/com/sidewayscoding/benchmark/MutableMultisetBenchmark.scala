467472ce0d692d38fa39fe01c0e1ff8b1ad3ce98
Mads Hartmann Jensen <mads379@gmail.com>
2012-11-12 21:38:01 +0100

    Renamed benchmark

--- src/test/scala/com/sidewayscoding/benchmark/MutableMultisetBenchmark.scala
+++ src/test/scala/com/sidewayscoding/benchmark/MutableMultisetBenchmark.scala
@@ -4,14 +4,12 @@ import com.sidewayscoding.mutable.FullMultiset
 import scala.collection.parallel.ForkJoinTaskSupport
 
 // runbench com.sidewayscoding.benchmark.MutableMultisetBench 100
-object MutableMultisetBench extends testing.Benchmark {
+object MutableMultisetBenchmark extends testing.Benchmark {
 
-  val length = 300
-  val partrie = FullMultiset((0 until length) zip (0 until length): _*)
+  val length = 2000
+  val ms = FullMultiset((0 until length): _*)
 
   def run = {
-    partrie map {
-      kv => kv
-    }
+    ms.size
   }
 }
